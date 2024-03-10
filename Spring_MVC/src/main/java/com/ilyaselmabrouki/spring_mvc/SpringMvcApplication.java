f2610280f869f5148de16dc7368c1069871dae54
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-03-10 21:35:15 +0000

    Generated dummy data and created patient view

--- Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/SpringMvcApplication.java
+++ Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/SpringMvcApplication.java
@@ -1,13 +1,35 @@
 package com.ilyaselmabrouki.spring_mvc;
 
+import com.ilyaselmabrouki.spring_mvc.entities.Patient;
+import com.ilyaselmabrouki.spring_mvc.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringMvcApplication {
+public class SpringMvcApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+		/*patientRepository.save(new Patient(null, "Patient1", new Date(), false, 10));
+		patientRepository.save(new Patient(null, "Patient2", new Date(), true, 20));
+		patientRepository.save(new Patient(null, "Patient3", new Date(), false, 30));*/
+
+        List<Patient> patientsV1 = patientRepository.findAll();
+        System.out.println("Liste des patients:");
+        patientsV1.forEach(p -> {
+            System.out.println(p.toString());
+        });
     }
 
 }
---
+++ Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/web/PatientController.java
@@ -0,0 +1,28 @@
+package com.ilyaselmabrouki.spring_mvc.web;
+
+import com.ilyaselmabrouki.spring_mvc.entities.Patient;
+import com.ilyaselmabrouki.spring_mvc.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @RequestMapping(value = "/index")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "5") int s){
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(p, s, Sort.unsorted()));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        return "patients";
+    }
+}
