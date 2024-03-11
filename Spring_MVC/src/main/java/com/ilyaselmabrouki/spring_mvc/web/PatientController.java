1341dff13284faff3faf387be2da474d4249bb8b
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-03-11 22:39:36 +0000

    Implemented pagination

--- Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/web/PatientController.java
+++ Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/web/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,12 +18,14 @@ public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
 
-    @RequestMapping(value = "/index")
+    @GetMapping(value = "/index")
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "0") int p,
                         @RequestParam(name = "size", defaultValue = "5") int s){
         Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(p, s, Sort.unsorted()));
         model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", p);
         return "patients";
     }
 }
