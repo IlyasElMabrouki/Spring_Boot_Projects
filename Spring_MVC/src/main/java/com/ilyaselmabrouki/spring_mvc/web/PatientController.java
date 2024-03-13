45ac9934eb1f0233700f32a2389b5c2e2e77bf82
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-03-13 17:38:10 +0000

    Added edit functionality

--- Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/web/PatientController.java
+++ Spring_MVC/src/main/java/com/ilyaselmabrouki/spring_mvc/web/PatientController.java
@@ -33,22 +33,37 @@ public class PatientController {
         return "patients";
     }
 
-    @GetMapping(value = "/delete")
-    public String delete(Long id, String keyword, int page){
-        patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
-    }
-
     @GetMapping(value = "/formPatient")
     public String formPatient(Model model){
         model.addAttribute("patient", new Patient());
         return "formPatient";
     }
 
+    @GetMapping(value = "/delete")
+    public String delete(Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/edit")
+    public String edit(Model model, Long id, String keyword, int page){
+        Patient p = patientRepository.findById(id).orElse(null);
+        if(p == null) throw new RuntimeException("Not Founded Patient");
+        model.addAttribute("patient",p);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
+
     @PostMapping(path = "/save")
-    public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
+    public String save(Model model,
+                       @RequestParam(name = "page", defaultValue = "0") int page,
+                       @RequestParam(name = "keyword", defaultValue = "") String keyword,
+                       @Valid Patient patient,
+                       BindingResult bindingResult){
         if (bindingResult.hasErrors()) return "formPatient";
         patientRepository.save(patient);
-        return "redirect:/formPatient";
+        return "redirect:/index?page="+page+"&keyword="+keyword;
     }
+
 }
